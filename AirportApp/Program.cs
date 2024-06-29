20d9e45c933200f9b4091f3a7c0bb2b97ba1e5be
yalvam <yashashwini.alva@outlook.com>
2024-06-29 22:17:09 +0100

    comment 2

--- AirportApp/Program.cs
+++ AirportApp/Program.cs
@@ -3,7 +3,7 @@ namespace AirportApp
     internal static class Program
     {
         /// <summary>//
-        ///  The main entry point for the application.
+        ///  The main entry point for the application.//
         /// </summary>
         [STAThread]
         static void Main()
